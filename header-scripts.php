ac82facfa4b4cfe61ae791bcd6cf69c2e833ff20
PradatiusD <danielprada2012@gmail.com>
2013-09-08 21:03:48 -0400

    updated icons and layout for services page.  Also added original header scripts

---
+++ header-scripts.php
@@ -0,0 +1,14 @@
+	<meta name="viewport" content="width=device-width">
+	<meta name="Description" lang="en" content="Description">	
+<meta name="apple-mobile-web-app-title" content="Napoleon">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="Icon-72@2x-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="Icon@2x-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="Icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+	<link rel="shortcut icon" href="../favicon.ico">
+
+<script type="text/javascript" src="//use.typekit.net/pve4mhm.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.js"></script>
